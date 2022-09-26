45a6fecb82874f7be795900f6ba446978d1c6ce2
elderberry17 <bezgin14a@mail.ru>
2022-09-26 14:57:58 +0300

    tried to fix smth

--- python/lesson3/hw3.py
+++ python/lesson3/hw3.py
@@ -1,7 +1,9 @@
 import json
 
 
-# colours: https://ozzmaker.com/add-colour-to-text-in-python/
+# достаточно костыльно реализовал DoD
+# расклеился после среды и не попробовал переписать через отдельный класс
+# надеюсь получить фидбек и осознать, как можно решить такую задачу лучше
 
 
 class ColorizeMixin:
@@ -10,7 +12,7 @@ class ColorizeMixin:
 
 
 class Advert(ColorizeMixin):
-    def __init__(self, mapping):
+    def __init__(self, mapping: json):
         super().__init__()
         self.mapping = mapping
         # take a look if 'price' is in a higher scope and work with this key
@@ -21,23 +23,24 @@ class Advert(ColorizeMixin):
                 elif self.mapping['price'] < 0:
                     raise ValueError('must be >= 0')
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str) -> object:
         if isinstance(self.mapping.get(item), dict):
             return self.__class__(self.mapping.get(item))
         return self.__class__(self.mapping.get(item)).mapping
 
     # here is a question with colour
     def __repr__(self):
-        return f'\033[0;{self.repr_color_code};48m{self.mapping["title"]} | {self.mapping["price"]} ₽'
+        if 'title' in self.mapping:
+            return f'\033[0;{self.repr_color_code};48m{self.mapping["title"]} | {self.mapping["price"]} ₽'
+        return f'{self.mapping}'
 
 
 lesson_str = """{
     "title": "python", "price": 0,
     "location": {
         "address": "город Москва, Лесная, 7",
-        "metro_stations": {"A": "Белорусская"}
-    },
-    "price": 12
+        "metro_stations": ["Белорусская"]
+    }
 }"""
 
 if __name__ == "__main__":
@@ -45,8 +48,8 @@ if __name__ == "__main__":
     lesson_ad = Advert(lesson)
 
     print(lesson)
-    print(lesson_ad.location.metro_stations.A)
-    print(lesson_ad.title, lesson_ad.price)
+    print(lesson_ad.location)
+    print(lesson_ad.location.metro_stations)
     print(lesson_ad)
     lesson_ad.repr_color_code = 35
     print(lesson_ad)
